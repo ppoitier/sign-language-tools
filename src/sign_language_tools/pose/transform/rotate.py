234aad9de2ce06b466ed3518d545511449c74443
Jérôme Fink <jerome.fink@unamur.be>
2023-08-10 10:36:09 +0200

    Code formating

--- src/sign_language_tools/pose/transform/rotate.py
+++ src/sign_language_tools/pose/transform/rotate.py
@@ -6,7 +6,6 @@ from sign_language_tools.core.transform import Transform
 
 
 class Rotation2D(Transform):
-
     def __init__(self, angle: float, center=(0.5, 0.5)):
         super().__init__()
         self.center = center
@@ -23,7 +22,7 @@ class Rotation2D(Transform):
 
 
 class RandomRotation2D(Rotation2D):
-    def __init__(self, angle_range=(0, 2*pi), center=(0.5, 0.5)):
+    def __init__(self, angle_range=(0, 2 * pi), center=(0.5, 0.5)):
         r0, r1 = angle_range
         angle = random.uniform(r0, r1)
         super().__init__(angle, center)
