646fa8184669e314f24197e7da7533763a00a907
ppoitier <pierre.poitier@unamur.be>
2023-08-11 09:20:44 +0200

    Fix compose transform

--- src/sign_language_tools/common/transforms/compose.py
+++ src/sign_language_tools/common/transforms/compose.py
@@ -2,11 +2,17 @@ from sign_language_tools.core.transform import Transform
 
 
 class Compose(Transform):
-    def __init__(self, transforms: list[callable]):
+    def __init__(self, transforms: list[callable], multi_input: bool = False):
         super().__init__()
         self.transforms = transforms
+        self.multi_input = multi_input
 
     def __call__(self, *args):
-        for t in self.transforms:
-            args = t(*args)
+        if self.multi_input:
+            for t in self.transforms:
+                args = t(*args)
+        else:
+            args = args[0]
+            for t in self.transforms:
+                args = t(args)
         return args
