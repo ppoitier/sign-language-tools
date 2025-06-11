a913086bdafa49c211c4d98db25a10d622c2e2ac
ppoitier <hanofound@hormail.com>
2025-06-11 14:49:40 +0200

    Fix flip pose transform

--- src/sign_language_tools/pose/transform/flip.py
+++ src/sign_language_tools/pose/transform/flip.py
@@ -4,6 +4,11 @@ from sign_language_tools.core.transform import Transform
 
 
 class HorizontalFlip(Transform):
+    def __init__(self, origin: tuple[float, float] = (0.5, 0.5)):
+        super().__init__()
+        self.origin_x = origin[0]
+
     def __call__(self, landmarks: np.ndarray):
-        landmarks[:, :, 0] = 1 - landmarks[:, :, 0]
+        landmarks[:, :, 0] = -(landmarks[:, :, 0] - self.origin_x) + self.origin_x
+        # landmarks[:, :, 0] = 1 - landmarks[:, :, 0]
         return landmarks
